be64d050700a6f95417d3fdb9a7f2238a19be897
TheFalcon73 <ffalconz@gmail.com>
2025-10-24 16:55:59 -0600

    Add test cases
    add test cases

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Menu
 
-# Create your tests here.
+class MenuModelTest(TestCase):
+    def setUp(self):
+        self.menu_item = Menu.objects.create(
+            title="Test Dish",
+            price=12.99,
+            inventory=50
+        )
+    
+    def test_menu_creation(self):
+        """Test that menu item is created correctly"""
+        self.assertEqual(self.menu_item.title, "Test Dish")
+        self.assertEqual(self.menu_item.price, 12.99)
+        self.assertEqual(self.menu_item.inventory, 50)
